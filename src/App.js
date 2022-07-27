a292de19d6d3fcc2a9405cffbc45d3735f6bb6a7
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-27 07:45:24 +0530

    called props drilling

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <C1 name="Gaurav Nasa" />
+    </div>
+  );
+}
+
+function C1(props) {
+  return <C2 name={props.name} />;
+}
+
+function C2(props) {
+  return <C3 name={props.name} />;
+}
+function C3(props) {
+  return <C4 name={props.name} />;
+}
+function C4(props) {
+  return <h2>{props.name}</h2>;
+}
+
+export default App;
